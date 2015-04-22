d7fa6b955ddf4706887d8146c51c0058dccb2ecf
hp197 <draakje197@gmail.com>
2015-04-23 01:37:51 +0200

    First fixes

---
+++ config/combiner.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'javascript'	=>	[
+		'enabled'		=>	true,
+		'path'			=>	'resources/assets/js/',
+		'route'			=>	'_js',
+	],
+
+	'css'			=>	[
+		'enabled'		=>	true,
+		'path'			=>	'resources/assets/css/',
+		'route'			=>	'_css',
+	],
+];
