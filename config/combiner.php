a77a8d590852b48648bdfce9bce4db1548115ab5
hp197 <hp197@users.noreply.github.com>
2015-04-23 19:24:06 +0200

    Update combiner.php

--- config/combiner.php
+++ config/combiner.php
@@ -5,11 +5,13 @@ return [
 		'enabled'		=>	true,
 		'path'			=>	'resources/assets/js/',
 		'route'			=>	'_js',
+		'expires'		=>	(60 * 60 * 24 * 30), // 30 Days
 	],
 
 	'css'			=>	[
 		'enabled'		=>	true,
 		'path'			=>	'resources/assets/css/',
 		'route'			=>	'_css',
+		'expires'		=>	(60 * 60 * 24 * 30), // 30 Days
 	],
 ];
